1083bd8e29c628a27c557b88239df9bc50e809de
Sarah Fuoss <sfuoss@gmail.com>
2021-09-01 14:50:02 -0500

    class notes added with ajax

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,25 @@ $(readyNow);
 function readyNow(){
 console.log('client.js');
 }
+
+function getTheTrains() {
+    // Make a request to the server for the trains
+    $.ajax({
+        method: 'GET',
+        url: '/trains',
+    }).then(function(response) {
+        console.log('Success!', response);
+        
+    });
+    console.log('After ajax');
+    
+}
+
+// funciton(response) is an anonymous function that doesn't require a name because it only does something with the response
+
+// function handleSuccess(response) {
+//     // response = trains;
+//     console.log('success', response);
+//     // append the data to the DOM
+    
+// }
