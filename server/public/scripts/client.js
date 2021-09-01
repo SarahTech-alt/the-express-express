2a0aa12e4b3a9f069c60955a81544c8c76d393fa
Sarah Fuoss <sfuoss@gmail.com>
2021-09-01 15:49:39 -0500

    added class notes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,20 @@ $(readyNow);
 
 function readyNow(){
 console.log('client.js');
+// set up a click handler for the button
+$('#submit-button').on('click', postTheTrain);
+getTheTrains();
+}
+
+function postTheTrain() {
+    $.ajax({
+        method: 'POST',
+        url: '/trains',
+        data: {
+            name: $('#train-name').val(),
+            color: $('#train-color').val()
+        },
+    }).then(handlePostSuccess).catch(postError);
 }
 
 function getTheTrains() {
@@ -9,19 +23,45 @@ function getTheTrains() {
     $.ajax({
         method: 'GET',
         url: '/trains',
-    }).then(function(response) {
-        console.log('Success!', response);
-        
-    });
+    }).then(handleSuccess).catch(handleError); 
     console.log('After ajax');
-    
+    }
+
+function handlePostSuccess() {
+    // clear the inputs
+    $('input').val('');
+    getTheTrains();
 }
 
-// funciton(response) is an anonymous function that doesn't require a name because it only does something with the response
+function postError(){
+    // log and alert the error
+}
 
-// function handleSuccess(response) {
+function handleError(error) {
+    alert('Something went wrong!');
+}
+
+// function(response) is an anonymous function that doesn't require a name because it only does something with the response
+
+function handleSuccess(response) {
+    $('#train-depot').empty();
+    const listOfTrains = response;
+    for(const train of listOfTrains) {
+        console.log(train);
+        $('#train-depot').append(`<li>${train.name}: ${train.color}</li>`)
+    }
+} 
+
+
 //     // response = trains;
 //     console.log('success', response);
 //     // append the data to the DOM
-    
-// }
+
+   // console.log('Success!', response);
+        // const listOfTrains = response;
+        // for(const train of listOfTrains) {
+        //     console.log(train);
+        //     $('#train-depot').append(`<li>${train.name}: ${train.color}`)
+        // }
+        
+    // }).catch(handleError);
